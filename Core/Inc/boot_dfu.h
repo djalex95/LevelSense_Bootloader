2e4d63e4f6dd11b7477d3461a35c47f59551c6b2
djalex95 <a_haeckelmoser@yahoo.de>
2026-07-08 17:42:36 +0200

    Weiter auf 1.1.1-dev

--- Core/Inc/boot_dfu.h
+++ Core/Inc/boot_dfu.h
@@ -13,7 +13,7 @@
 
 /* Bootloader-Version. Wird im Update-Modus auf das Kommando "VER" als
  * "BLV;<version>\n" gemeldet. Bei jeder Bootloader-Freigabe erhöhen. */
-#define BL_VERSION  "1.1.0"
+#define BL_VERSION  "1.1.1-dev"
 
 /* Flash-Schnittstelle (auf dem Ziel per HAL, im Test per RAM-Puffer). */
 typedef struct {
